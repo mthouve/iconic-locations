2d1fd9c77f36396ba406d7aac9ffcdd234dd905d
Matt Thouvenot <matt.thouvenot@secondstreet.com>
2015-02-26 15:20:31 -0600

    Added helpers for previewing text and formatting dates

---
+++ tests/unit/helpers/date-formatter-test.js
@@ -0,0 +1,12 @@
+import {
+  dateFormatter
+} from '../../../helpers/date-formatter';
+import { module, test } from 'qunit';
+
+module('DateFormatterHelper');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = dateFormatter(42);
+  assert.ok(result);
+});
---
+++ tests/unit/helpers/text-preview-test.js
@@ -0,0 +1,12 @@
+import {
+  textPreview
+} from '../../../helpers/text-preview';
+import { module, test } from 'qunit';
+
+module('TextPreviewHelper');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = textPreview(42);
+  assert.ok(result);
+});
